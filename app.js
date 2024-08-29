9803ab9488871e8dedab5a45954bb5151006fa2e
Jacob Parry <54328477+Jacob-Vance@users.noreply.github.com>
2024-08-29 12:57:59 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const express = require('express')
 const app = express()
 
+app.use(express.static('./'))
+
+console.log('im on a node server change that and that tanad f, yo');
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
 })
 
-app.listen(3000)
+app.listen(5000)
